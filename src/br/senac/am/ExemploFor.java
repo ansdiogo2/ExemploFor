9cef725562e5ae7c493fea6fa24d25ec8f835934
ansdiogo2 <drs@cesar.school>
2026-04-25 11:20:49 -0400

    - Criada a Classe MinhasFuncoes para separar a logica de repeicao

--- src/br/senac/am/ExemploFor.java
+++ src/br/senac/am/ExemploFor.java
@@ -3,7 +3,7 @@ package br.senac.am;
 import java.util.Iterator;
 
 public class ExemploFor {
-	protected static String estadosDoAluno;
+	//protected static String estadosDoAluno;
 
 	public ExemploFor() {
 		// TODO Auto-generated constructor stub
@@ -11,39 +11,16 @@ public class ExemploFor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
-		System.out.println(estadosDoAluno);
+		System.out.println(MinhasFuncoes.estadosDoAluno);
 
-		imprimirNumeros();
-		imprimirParesComFor();
-		imprimirParesComWhile();
-		System.out.println(estadosDoAluno);
+		MinhasFuncoes.imprimirNumeros();
+		MinhasFuncoes.imprimirParesComFor();
+		MinhasFuncoes.imprimirParesComWhile();
+		
+		System.out.println(MinhasFuncoes.estadosDoAluno);
 
 	}
 	
-	private static void imprimirParesComWhile() {
-		// TODO Auto-generated method stub
-		int contadorPar = 0;
-		while(contadorPar <= 21) {
-			System.out.println("Número par: " + contadorPar);
-			contadorPar =contadorPar+2;
-		}
-	}
-
-	private static void imprimirNumeros() {
-		// Imprimir números pares de 1 a 9
-		estadosDoAluno = "Estado do Aluno Igor definido";
-		for(int i = 1; i <=9; i ++) {
-			System.out.println("Numero: "+i);
-		}
-		System.out.println("fim de loop");
-	}
-
-	private static void imprimirParesComFor() {
-		// Imprimir números pares de 0 a 21
-		for(int i = 0; i <=21; i=i+2) {
-			System.out.println("Numero: "+i);
-		}
-		System.out.println("fim de loop");
-	}
+	
 
 }
--- src/br/senac/am/MinhasFuncoes.java
+++ src/br/senac/am/MinhasFuncoes.java
@@ -3,6 +3,30 @@ package br.senac.am;
 public class MinhasFuncoes {
 	protected static String estadosDoAluno;
 	
-	
+	public static void imprimirParesComWhile() {
+		// TODO Auto-generated method stub
+		int contadorPar = 0;
+		while(contadorPar <= 21) {
+			System.out.println("Número par: " + contadorPar);
+			contadorPar =contadorPar+2;
+		}
+	}
+
+	public static void imprimirNumeros() {
+		// Imprimir números pares de 1 a 9
+		estadosDoAluno = "Estado do Aluno Igor definido";
+		for(int i = 1; i <=9; i ++) {
+			System.out.println("Numero: "+i);
+		}
+		System.out.println("fim de loop");
+	}
+
+	public static void imprimirParesComFor() {
+		// Imprimir números pares de 0 a 21
+		for(int i = 0; i <=21; i=i+2) {
+			System.out.println("Numero: "+i);
+		}
+		System.out.println("fim de loop");
+	}
 
 }
