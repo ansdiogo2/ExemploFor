ce4a1e908a58d6ff2730a419555aaf913691b7c2
ansdiogo2 <drs@cesar.school>
2026-04-25 10:29:31 -0400

    Uso simple de laço de repetição For

---
+++ src/br/senac/am/ExemploFor.java
@@ -0,0 +1,21 @@
+package br.senac.am;
+
+import java.util.Iterator;
+
+public class ExemploFor {
+
+	public ExemploFor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Imprimir números pares de 1 a 9
+		for(int i = 1; i <=9; i ++) {
+			System.out.println("Numero: "+i);
+		}
+		System.out.println("fim de loop");
+		
+	}
+
+}
