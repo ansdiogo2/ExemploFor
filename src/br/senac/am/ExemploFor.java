0a8382a9552005c0f486eea999897f1bd2823d91
ansdiogo2 <drs@cesar.school>
2026-04-25 11:02:58 -0400

    Adicionado o conceito de protect com aluno

--- src/br/senac/am/ExemploFor.java
+++ src/br/senac/am/ExemploFor.java
@@ -3,16 +3,21 @@ package br.senac.am;
 import java.util.Iterator;
 
 public class ExemploFor {
+	protected static String aluno;
 
 	public ExemploFor() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub	
+		System.out.println(aluno);
+
 		imprimirNumeros();
 		imprimirParesComFor();
 		imprimirParesComWhile();
+		System.out.println(aluno);
+
 	}
 	
 	private static void imprimirParesComWhile() {
@@ -24,15 +29,16 @@ public class ExemploFor {
 		}
 	}
 
-	public static void imprimirNumeros() {
+	private static void imprimirNumeros() {
 		// Imprimir números pares de 1 a 9
+		aluno = "Igor";
 		for(int i = 1; i <=9; i ++) {
 			System.out.println("Numero: "+i);
 		}
 		System.out.println("fim de loop");
 	}
 
-	public static void imprimirParesComFor() {
+	private static void imprimirParesComFor() {
 		// Imprimir números pares de 0 a 21
 		for(int i = 0; i <=21; i=i+2) {
 			System.out.println("Numero: "+i);
